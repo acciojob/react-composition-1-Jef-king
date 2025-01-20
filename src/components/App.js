d71a4f035f1d8dfb4e7f05d43409a8b0c3facb4d
Jef-king <147884623+Jef-king@users.noreply.github.com>
2025-01-20 20:17:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+export default function App() {
+  const [selectedTab, setSelectedTab] = useState('');
 
-const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Item setSelectedTab={setSelectedTab} />
+      <Result selectedTab={selectedTab} />
     </div>
-  )
+  );
 }
 
-export default App
+function Item({ setSelectedTab }) {
+  const handleClick = (event) => {
+    setSelectedTab(event.target.innerText);
+  };
+
+  return (
+    <ul>
+      <li onClick={handleClick}>Tab 1</li>
+      <li onClick={handleClick}>Tab 2</li>
+      <li onClick={handleClick}>Tab 3</li>
+    </ul>
+  );
+}
+
+function Result({ selectedTab }) {
+  if (selectedTab !== '') {
+    return <p>This is the content for {selectedTab}</p>;
+  } else {
+    return null;
+  }
+}
